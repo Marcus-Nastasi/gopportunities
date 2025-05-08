12f029b6147fa22f634019ea242db33a4dd57e04
Marcus-Nastasi <vinnie.nsts@gmail.com>
2025-05-08 12:15:09 -0300

    feat: completed schema

--- schemas/opening.go
+++ schemas/opening.go
@@ -4,4 +4,10 @@ import "gorm.io/gorm"
 
 type Opening struct {
 	gorm.Model
+	Role     string  `json:"role"`
+	Company  string  `json:"company"`
+	Location string  `json:"location"`
+	Remote   bool    `json:"remote"`
+	Link     string  `json:"link"`
+	Salary   float64 `json:"salary"`
 }
