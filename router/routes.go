c83f1e8678e0869c164b430cc81654f47b9846c1
Marcus-Nastasi <vinnie.nsts@gmail.com>
2025-05-08 11:12:12 -0300

    feat: created base routes

--- router/routes.go
+++ router/routes.go
@@ -12,5 +12,24 @@ func routes(server *gin.Engine) {
 		v1.GET("/status", func(ctx *gin.Context) {
 			ctx.JSON(200, map[string]string{"status": "ok"})
 		})
+
+		v1.GET("",func(ctx *gin.Context) {
+			ctx.JSON(200, map[string]string{"get": "all"})
+		})
+		v1.GET("/:id",func(ctx *gin.Context) {
+			id := ctx.Param("id")
+			ctx.JSON(200, map[string]string{"get": id})
+		})
+		v1.POST("",func(ctx *gin.Context) {
+			ctx.JSON(201, map[string]string{"post": "create"})
+		})
+		v1.PATCH("/:id",func(ctx *gin.Context) {
+			id := ctx.Param("id")
+			ctx.JSON(200, map[string]string{"update": id})
+		})
+		v1.DELETE("/:id", func(ctx *gin.Context) {
+			id := ctx.Param("id")
+			ctx.JSON(200, map[string]string{"delete": id})
+		})
 	}
 }
