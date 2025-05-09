28c01939fffafb0160fcbb94108cfd07ef15dfc6
Marcus-Nastasi <vinnie.nsts@gmail.com>
2025-05-09 19:22:22 -0300

    fix: refactoring response

--- handler/output/opportunitie_response.go
+++ handler/output/opportunitie_response.go
@@ -3,14 +3,14 @@ package output
 import "time"
 
 type OpportunitieResponse struct {
-	ID			 	 uint  `json:"id"`
-	CreatedAt  time.Time  `json:"createdAt"`
-	UpdatedAt  time.Time	`json:"updatedAt"`
+	ID			 	 uint  			 `json:"id"`
+	CreatedAt  time.Time   `json:"createdAt"`
+	UpdatedAt  time.Time	 `json:"updatedAt"`
 	DeletedAt  *time.Time  `json:"deletedAt"`
-	Role     	 string  `json:"role"`
-	Company  	 string  `json:"company"`
-	Location 	 string  `json:"location"`
-	Remote   	 bool    `json:"remote"`
-	Link     	 string  `json:"link"`
-	Salary   	 float64 `json:"salary"`
+	Role     	 string  		 `json:"role"`
+	Company  	 string  		 `json:"company"`
+	Location 	 string  		 `json:"location"`
+	Remote   	 bool    		 `json:"remote"`
+	Link     	 string  		 `json:"link"`
+	Salary   	 float64 		 `json:"salary"`
 }
